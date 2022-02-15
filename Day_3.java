6b5b07caf93f375cc416a635636b4e4eef9f74a9
Darja Port <darja.port@gmail.com>
2022-02-15 15:24:43 +0100

    Day 3 - Second star

--- Day_3.java
+++ Day_3.java
@@ -3,6 +3,61 @@ import java.util.*;
 
 public class Day_3 {
 
+    public static int secondStar(ArrayList<String> list) {
+        ArrayList<String> tmpOxygen = new ArrayList<>(list);
+        ArrayList<String> tmpCO2 = new ArrayList<>(list);
+        int oxygen = 0;
+        int co2 = 0;
+        int i = 0;
+
+        while (i < list.get(0).length()) {
+            int z = i;
+            int zero = 0;
+            int one = 0;
+
+            for (String x : tmpOxygen) {
+                if (x.charAt(i) == '0') {
+                    zero++;
+                } else if (x.charAt(i) == '1') {
+                    one++;
+                }
+            }
+
+            if (tmpOxygen.size() > 1) {
+                if (zero == one || zero < one) {
+                    tmpOxygen.removeIf(y -> y.charAt(z) == '0');
+                } else {
+                    tmpOxygen.removeIf(y -> y.charAt(z) == '1');
+                }
+            }
+
+            zero = 0;
+            one = 0;
+            for (String x : tmpCO2) {
+                if (x.charAt(i) == '0') {
+                    zero++;
+                } else if (x.charAt(i) == '1') {
+                    one++;
+                }
+            }
+
+            if (tmpCO2.size() > 1) {
+                if (zero == one || zero < one) {
+                    tmpCO2.removeIf(y -> y.charAt(z) == '1');
+                } else {
+                    tmpCO2.removeIf(y -> y.charAt(z) == '0');
+                }
+            }
+
+            i++;
+        }
+
+        oxygen = Integer.parseInt(tmpOxygen.get(0), 2);
+        co2 = Integer.parseInt(tmpCO2.get(0), 2);
+
+        return oxygen * co2;
+    }
+
     public static int firstStar(ArrayList<String> list) {
         int gamma = 0;
         int epsilon = 0;
@@ -58,6 +113,7 @@ public class Day_3 {
         }
 
         System.out.println("First star: " + firstStar(input));
+        System.out.println("Second star: " + secondStar(input));
 
     }
 
